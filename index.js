43f98bc820dd6e40ef76bab12f6a0da9a05fea5c
Zac Willmington <zac.willmington@gmail.com>
2018-09-16 23:03:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function produceDrivingRange(blockRange){
+    return function(start, end){
+        let blockStartNumberString = start.split("th").join();
+        let blockStartInt = parseInt(blockStartNumberString, 10);
+
+        let blockEndNumberString = end.split("th").join();
+        let blockEndInt = parseInt(blockEndNumberString, 10);
+
+        let blockDifference = blockEndInt - blockStartInt;
+        if (blockDifference - blockRange > 0){
+            return `${blockDifference - blockRange} blocks out of range`;
+        } else {
+            return `within range by ${blockRange - blockDifference}`;
+        }
+    }
+}
